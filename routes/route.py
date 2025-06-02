be23f0cb87ef2c97b593269cac56693615b5d48a
yyhago <yhago.felipe.teles@gmail.com>
2025-06-02 13:26:19 -0300

    Finalization of my code routes

--- routes/route.py
+++ routes/route.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from typing import Optional, List
 
-from database.db import livro_db
+from database.db import livro_db,add_livro
 
 appRouter = APIRouter()
 
@@ -18,17 +18,31 @@ class Livro(BaseModel):
 def livros():
     return livro_db 
 
-
+    
 @appRouter.post("/adicionarlivro")
 def adicionar_livro(livro: Livro):
-    pass
+    try:
+        livro_dict = livro.dict()
+        novo_livro = add_livro(livro_dict)
+        return {"mensagem": "Livro adicionado com sucesso", "livro": novo_livro}
+    except Exception as erro:
+        return {"mensagem": erro}
 
 
 @appRouter.put("/atualizarlivro/{id}")
-def atualizar_livro(id: int):
-    pass
+def atualizar_livro(id: int, livro: Livro):  
+    for i, l in enumerate(livro_db):
+        if l["id"] == id:
+            livro_db[i] = livro.dict() 
+            return {"mensagem": "Livro atualizado com sucesso", "livro": livro_db[i]}
+    return {"erro": "Livro não encontrado"}
+
 
 
 @appRouter.delete("/deletarlivro/{id}")
 def deletar_livro(id: int):
-    pass
+    for i, l in enumerate(livro_db):
+        if l["id"] == id:
+            livro_db.pop(i)
+            return {"mensagem": "Livro deletado com sucesso"}
+    return {"erro": "Livro não encontrado"}
